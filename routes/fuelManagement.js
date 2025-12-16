d124d14810a21014b5a03b7bd27cc9374ca58933
neel281998 <neel281998@gmail.com>
2025-12-16 18:18:15 +0530

    feat: Integrate mongoose in fuelManagement.js - Added mongoose import to facilitate database interactions, laying the groundwork for future enhancements in fuel management functionality.

--- routes/fuelManagement.js
+++ routes/fuelManagement.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const MainStorage = require('../models/MainStorage');
 const SubPump = require('../models/SubPump');
 const FuelRestock = require('../models/FuelRestock');
